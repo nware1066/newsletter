f83ef7ec19313d0a6d5da0983024903d05029887
Naomi Ware <nware1066@yahoo.com>
2021-10-29 18:16:57 -0700

    Create basic html formatting in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <header className="header">
+      <h1 className="header-title">Stitching Circle</h1>
+     </header>
+     <main className="newsletter-main">
+      <h1 className="main-title">Hello Friends</h1>
+      <p className="newsletter-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus maiores nesciunt inventore rem deserunt, vitae architecto sunt cumque veniam, sit ea sed praesentium eveniet illum corporis debitis doloremque quidem blanditiis?</p>
+      <p className="newsletter-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt odit sunt voluptate minima ipsa asperiores nobis quos, sit quis. Facere, error numquam! Nisi ea cupiditate doloribus omnis. Sit ex ea at fugit aliquam debitis nihil fuga molestias dolores, odit quos qui minima architecto quas dolorem?</p>
+     </main>
     </div>
   );
 }
